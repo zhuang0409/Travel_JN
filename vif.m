3b57ecf5fb7dc4557d43e7e0dd2b753a40f525ed
zhuang0409 <60935986+zhuang0409@users.noreply.github.com>
2023-08-07 17:25:00 +0200

    Update vif.m

--- vif.m
+++ vif.m
@@ -13,7 +13,12 @@ x=reshape(super.dsm,[5184,1]);
 y=reshape(basic.dsm,[5184,1]);
 z=reshape(sub.dsm,[5184,1]);
 
-%% VIFs: three taxonomic levels and the visual model
+%% VIFs: three taxonomic levels, the visual model and place RDM
+% places
+load('places_last2nd.mat');
+placesdsm=targetlayer_dsm;
+places=reshape(placesdsm,[5184,1]);
+
 % addpath('/Users/zhuang/EEGNet/results_ica/npy-matlab-master/npy-matlab');
 modeldir='G:\Travel\data\Resnet50\outputs\';
 matrixnames=dir([modeldir,'*.npy']);
@@ -21,16 +26,16 @@ data= readNPY([modeldir,matrixnames(1).name]);
 modeldsm=VGG_reshape_to_fMRI(squeeze(data(1,:,:)));
 modelds=reshape(modeldsm,[5184,1]);
 
-TT=[x y z modelds];r_dnn=corrcoef(TT);vif_dnn=diag(inv(r_dnn));
+T_levels=[x, y, z, modelds places];h_levels=corrcoef(T_levels);vif_levels=diag(inv(h_levels));
 
-%% VIFs: behavioral rdm, one of three taxonomic levels, and the visual model.
+%% VIFs: behavioral rdm, one of three taxonomic levels, the visual model and place RDM..
 load('MDS.mat');
 
 humanRDM=VGG_reshape_to_fMRI(kron(matrix,ones(6,6)));
 h=reshape(humanRDM,[5184,1]);
 %z-sub
-t1=[h z modelds];r1=corrcoef(t1);vif1_mds=diag(inv(r1));
+t1=[h z modelds, places];r1=corrcoef(t1);vif1_mds=diag(inv(r1));
 %y-basic
-t2=[h y modelds];r2=corrcoef(t2);vif2_mds=diag(inv(r2));
+t2=[h y modelds, places];r2=corrcoef(t2);vif2_mds=diag(inv(r2));
 %x-super
-t3=[h x modelds];r3=corrcoef(t3);vif3_mds=diag(inv(r3));
+t3=[h x modelds, places];r3=corrcoef(t3);vif3_mds=diag(inv(r3));
